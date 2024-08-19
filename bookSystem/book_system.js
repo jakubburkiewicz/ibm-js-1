eb6d920d378ff8af5ce54eb65c82940915025bc8
Jakub Burkiewicz <jakub.burkiewicz@gmail.com>
2024-08-19 11:27:46 -0400

    Add deleting book functionality

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -28,7 +28,8 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deletebook(${index})">Delete</button>`
     );
 
     document.getElementById('books').innerHTML = booksDiv.join('');
@@ -41,3 +42,8 @@ function clearInputs() {
     document.getElementById('pagesNumber').value = '';
 }
 
+function deletebook( index ) {
+    books.splice( index, 1 )
+
+    showbooks()
+}
